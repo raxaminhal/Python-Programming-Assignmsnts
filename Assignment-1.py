8ee50641dabe1c48a5e910bc18e7c3be9b7bd5fe
raxaminhal <57320934+raxaminhal@users.noreply.github.com>
2019-11-05 12:17:05 +0500

    Python Assigment#1 Sylani Batch-2

--- Assignment-1.py
+++ Assignment-1.py
@@ -1,13 +1,55 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import datetime
 now = datetime.datetime.now()
 print ("Current date and time : ")
 print(now)
+
+
+# In[1]:
+
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+# In[2]:
+
+
+import sys
+print("Python version")
+print (sys.version)
+
+
+# In[4]:
+
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print (lname + " " + fname)
+
+
+# In[6]:
+
+
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+ 
+sum = a + b
+print(sum)
+ 
+
+
+# In[7]:
+
+
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
 
 
 # In[ ]:
